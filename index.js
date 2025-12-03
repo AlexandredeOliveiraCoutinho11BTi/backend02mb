90d96a213ee14a7289aff750d9a3f2faea6b91fd
AlexandredeOliveiraCoutinho11BTi <162030541+AlexandredeOliveiraCoutinho11BTi@users.noreply.github.com>
2025-12-03 20:30:53 -0300

    edutec!!!!

--- index.js
+++ index.js
@@ -1,76 +1,13 @@
 import express from "express"
 import cors from "cors"
-import mysql2  from "mysql2"
+import mysql2 from "mysql2"
+import serverless from "serverless-http";
 
-
-const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD} = process.env
-
+const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env
 
 const app = express()
-const port = 3333
-
-
-
 app.use(cors())
 app.use(express.json())
-
-app.get("/", (request, response) =>{
-    const selectCommand = "SELECT name, email FROM alexandrecoutinho_02mb"
-
-    database.query(selectCommand, (error, users) => {
-        if (error){
-            console.log(error)
-            return
-        }
-
-        response.json(users)
-    })
-})
-
-app.post("/login", (request, response ) => {
-    const {email, password} = request.body
-
-    const selectCommand = "SELECT * FROM alexandrecoutinho_02mb WHERE email = ?"
-
-    database.query(selectCommand, [email], (error, user) => {
-        if (error) {
-            console.log(error)
-            return
-        }
-
-        if (user.length === 0 || user[0].password !== password) {
-            response.json({ message: "Usuário ou Senha incorretos." })
-            return
-        }
-
-        response.json({id: user[0].id, name:user[0].name })
-    })
-})
-
-app.post("/cadastrar",  (request, response) => {
-
-    const  user  = request.body
-    console.log(user)
-
-    const insertCommand = `
-        INSERT INTO alexandrecoutinho_02mb(name, email, password)
-        VALUES(?, ?, ?)
-    `
-
-    database.query(insertCommand, [user.name, user.email, user.password], (error) =>{
-        if (error) {
-            console.log(error)
-            return
-        }
-
-         response.status(201).json ({message: "usuario cadastrado com sucesso"})
-    })
-
-})
-
-app.listen(port, () => {
-    console.log(`Server running on port ${3333}!`)
-})
 
 const database = mysql2.createPool({
     host: DB_HOST,
@@ -79,3 +16,48 @@ const database = mysql2.createPool({
     password: DB_PASSWORD,
     connectionLimit: 10
 })
+
+app.get("/", (request, response) => {
+    const selectCommand = "SELECT name, email FROM Evexplorer_edutec"
+    database.query(selectCommand, (error, users) => {
+        if (error) return response.status(500).json(error)
+        response.json(users)
+    })
+})
+
+app.post("/login", (request, response) => {
+    const { email, password } = request.body.user
+
+    const selectCommand = "SELECT * FROM Evexplorer_edutec WHERE email = ?"
+    database.query(selectCommand, [email], (error, user) => {
+        if (error) {
+        response.status(500).json(error)
+        return
+        }
+        if (user.length === 0 || user[0].password !== password) {
+    return response.json({ message: "Usuário ou senha incorretos!" });
+}
+
+
+        response.json({ id: user[0].id, name: user[0].name })
+    })
+})
+
+app.post("/cadastrar", (request, response) => {
+    const { user } = request.body
+
+    const insertCommand = `
+        INSERT INTO Evexplorer_edutec (name, email, password)
+        VALUES (?, ?, ?)
+    `
+
+    database.query(insertCommand, [user.name, user.email, user.password], (error) => {
+        if (error) {
+             response.status(500).json(error)
+            return
+        }
+        response.status(201).json({ message: "Usuário cadastrado com sucesso" })
+    })
+})
+
+export default serverless(app);
